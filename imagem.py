fbd71e42b85b018e26a85b7b57deb0ef2b0e69d4
Chavez-cu <phbbpedrobueno@usp.br>
2025-05-14 17:36:30 -0300

    todos os filtros já aplicados; falta linha para  baixar as imagens

--- imagem.py
+++ imagem.py
@@ -1,39 +1,56 @@
 import numpy as np
 import scipy
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gs
 import scipy.fftpack as fp
+import gauss
  
 
 g = np.genfromtxt('incognita-fft.txt', dtype = complex)
 
-F = scipy.fftpack.ifft2(g)
-F = np.absolute(F)
-
-plt.figure ()
-plt.imshow(F, cmap='gray')
+F1 = scipy.fftpack.ifft2(g)
+F1 = np.absolute(F1)
 
 
-############
-
-def gaussian(fc, sigma =1, vmax = .01):
-    Nu, Nv = fc.shape
-    u = Nu * np.linspace(-vmax, vmax, Nu)
-    v = Nv * np.linspace(-vmax, vmax, Nv)
-    U, V = np.meshgrid(v, u)
-
-    sigma2 = sigma**2
-    G = (np.exp(-(U*U + V*V) /(2. *sigma2)))/(2*np.pi*sigma2)
-    G = fp.fftshift(G)
-
-    return G
+M = gauss.gaussian_baixas(g, sigma=1) #Alterar valores de sigma: 1; .5; .1
+f1 = g * M
+F2 = scipy.fftpack.ifft2(f1)
+F2 = np.absolute(F2)
 
 
-M = gaussian(g, sigma=.1)
-f = g * M
-F = scipy.fftpack.ifft2(f)
-F = np.absolute(F)
+W = gauss.gaussian_altas(g, sigma=1) #Alterar valores de sigma: 1; .5; .1
+f2 = g * W
+F3 = scipy.fftpack.ifft2(f2)
+F3 = np.absolute(F3)
 
 
-plt.figure()
-plt.imshow(F, cmap= 'gray')
+##Plot
+fig = plt.figure(figsize=[10,4])
+grid = gs.GridSpec(1,3)
+
+ax1 = fig.add_subplot(grid[:,0])
+ax2 = fig.add_subplot(grid[:,1])
+ax3 = fig.add_subplot(grid[:,2])
+
+
+
+#imagem original
+ax1.imshow(F1, cmap='gray')
+ax1.axis('off')
+ax1.set_title('Imagem sem filtros')
+
+#Filtro passa-baixas
+
+ax2.imshow(F2, cmap='gray')
+ax2.axis('off')
+ax2.set_title('Filtro passa-baixas')
+
+#filtro passa-altas
+
+ax3.imshow(F3, cmap= 'gray')
+ax3.axis('off')
+ax3.set_title('Filtro passa-altas')
+
+
+plt.tight_layout() #para distribuir melhor o espaço
 plt.show()
