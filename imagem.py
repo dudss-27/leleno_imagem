5cb373ab223e11116bc87cf6acce51a95f1459c2
Chavez-cu <phbbpedrobueno@usp.br>
2025-05-14 16:20:23 -0300

    organizei o código para que o filtro fosse aplicado com sucesso; corrigi o plot da imagem; falta o passa-altas

--- imagem.py
+++ imagem.py
@@ -5,29 +5,35 @@ import scipy.fftpack as fp
  
 
 g = np.genfromtxt('incognita-fft.txt', dtype = complex)
+
 F = scipy.fftpack.ifft2(g)
 F = np.absolute(F)
 
 plt.figure ()
-plt.imshow(F, cmap='twilight')
-plt.show()
+plt.imshow(F, cmap='gray')
+
 
 ############
 
-def gaussian(F, sigma =1, vmax = .5):
-    Nu, Nv = F.shape
+def gaussian(fc, sigma =1, vmax = .01):
+    Nu, Nv = fc.shape
     u = Nu * np.linspace(-vmax, vmax, Nu)
     v = Nv * np.linspace(-vmax, vmax, Nv)
     U, V = np.meshgrid(v, u)
 
     sigma2 = sigma**2
-    G = np.exp(-(U*U + V*V) /2. /sigma2)
+    G = (np.exp(-(U*U + V*V) /(2. *sigma2)))/(2*np.pi*sigma2)
     G = fp.fftshift(G)
 
-    return G/sigma2
+    return G
 
-M = gaussian.gaussian(F, sigma=1)
-f_inversa = F * M
+
+M = gaussian(g, sigma=.1)
+f = g * M
+F = scipy.fftpack.ifft2(f)
+F = np.absolute(F)
+
+
 plt.figure()
-plt.plot(f_inversa)
+plt.imshow(F, cmap= 'gray')
 plt.show()
